d754b8c9977fc0f437f67a9aad5ef64bc8f52e60
Danny Villeda <1341188@crosemont.qc.ca>
2020-11-28 15:40:15 -0500

    Commit Inital

---
+++ ServicePatient/ServicePatient/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ServicePatient.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ServicePatient/ServicePatient/Models/Patient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServicePatient.Models
+{
+    [DataContract]
+    public class Patient
+    {
+        [DataMember]
+        int id { get; set; }
+        [DataMember]
+        string nom { get; set; }
+        [DataMember]
+        string prenom { get; set; }
+        [DataMember]
+        int age { get; set; }
+        [DataMember]
+        string sexe { get; set; }
+        [DataMember]
+        string allergies { get; set; }
+        [DataMember]
+        string adresse { get; set; }
+        [DataMember]
+        int num_tel { get; set; }
+        [DataMember]
+        bool assurance { get; set; }
+    }
+}
