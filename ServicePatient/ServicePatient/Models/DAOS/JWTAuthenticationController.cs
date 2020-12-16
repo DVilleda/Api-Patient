807b1672708dbc1143bcd7a9aabb8cadba1c89e7
gugui <guillaumel.20000@gmail.com>
2020-12-16 10:18:57 -0500

    rajout des token d'identification

---
+++ ServicePatient/ServicePatient/Models/DAOS/JWTAuthenticationController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServicePatient.Models.DAOS
+{
+    public class JWTAuthentication
+    {
+        private static string SecretKey = "TW9zaGVFcmV6UHKiolF0sopeFQ==";
+
+        public string GenererToken(int id, string typeUtilisateur)
+        {
+            string laCle = "";
+            byte[] key = Convert.FromBase64String(SecretKey);
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.NameIdentifier, id.ToString()),
+                    new Claim(ClaimTypes.Actor, typeUtilisateur)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(securityKey,
+                  SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+            return handler.WriteToken(token);
+        }
+
+        public static ClaimsPrincipal GetPrincipal(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
+                if (jwtToken == null)
+                    return null;
+                byte[] key = Convert.FromBase64String(SecretKey);
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+                SecurityToken securityToken;
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(token,
+                      parameters, out securityToken);
+                return principal;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public int DécoderTokenPourId(string token)
+        {
+            int id = -1;
+            ClaimsPrincipal principal = GetPrincipal(token);
+            if (principal == null)
+                return -1;
+            ClaimsIdentity identity = null;
+            try
+            {
+                identity = (ClaimsIdentity)principal.Identity;
+            }
+            catch (NullReferenceException)
+            {
+                return -1;
+            }
+
+            Claim idClaim = identity.FindFirst(ClaimTypes.NameIdentifier);
+            id = Int32.Parse(idClaim.Value);
+            return id;
+        }
+
+        public string DécoderTypeUtilisateur(string token)
+        {
+            string type = null;
+            ClaimsPrincipal principal = GetPrincipal(token);
+            if (principal == null)
+                return null;
+            ClaimsIdentity identity = null;
+            try
+            {
+                identity = (ClaimsIdentity)principal.Identity;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+
+            Claim idClaim = identity.FindFirst(ClaimTypes.Actor);
+            type = idClaim.Value;
+            return type;
+        }
+    }
+}
